8298487a5be6492d81275bac4cbce9a53f0949ef
Henrique-O-Santana <henriqueosantana06@gmail.com>
2025-10-01 00:08:52 -0300

    commit teste - Desafio

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n1,n2;
+        Scanner sc = new Scanner(System.in);
+        Contador contador = new Contador();
+
+        System.out.println("Digite o primeiro numero:");
+        n1 = sc.nextInt();
+
+        System.out.println("Digite o segundo numero:");
+        n2 = sc.nextInt();
+
+        try {
+            contador.contador(n1,n2);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");;
+        }
+    }
+
+}
